45242eaa454c3926050d575276697e235134d620
Patoliya Henish <henishpatoliya1508@gmail.com>
2024-10-03 01:09:56 +0530

    setup, login and sign up page and Header component

---
+++ client/src/pages/Chat.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppLayout from '../components/layout/AppLayout';
+
+const Chat = () => {
+  return (
+    <div>
+      chat
+    </div>
+  );
+}
+
+export default AppLayout()(Chat);
